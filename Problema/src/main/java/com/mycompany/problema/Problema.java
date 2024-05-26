def9779b6b158d841eabb4f6b69ec75111e6f075
Luis <Luis@192.168.1.45>
2024-05-26 14:23:31 +0200

    Main

---
+++ Problema/src/main/java/com/mycompany/problema/Problema.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema;
+
+import static com.mycompany.problema.Utils.*;
+
+/**
+ *
+ * @author Luis
+ */
+public class Problema {
+
+    public static void main(String[] args) {
+        afegirReptils();
+        ComprobarReptil();
+    }
+}
